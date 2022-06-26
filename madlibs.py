95ea1076a4379d6ac70e1d2bb96a3c5614a633c8
smritiislur <smritivenkatislur@gmail.com>
2022-06-26 19:06:07 +0530

    first commit

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adj=input("Adjective: ")
+vb1=input("Verb: ")
+vb2=input("Verb: ")
+print(f"I love to play {adj} games!! I love to {vb1}. I also like to {vb2}. what about you?")
+#yt="smriti"
+#print(f"subscribe to {yt}")
